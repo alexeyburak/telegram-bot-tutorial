8f236e7d9df919285983cef2911f17a8488df9d6
Alexey Burak <byraklesha@gmail.com>
2022-05-29 19:06:06 +0300

    feat: add saving files lesson

--- src/com/alexeyburak/lesson5/HandleToLessonFive.java
+++ src/com/alexeyburak/lesson5/HandleToLessonFive.java
@@ -1,23 +1,24 @@
-### Introduction
+package com.alexeyburak.lesson5;
 
-In this lesson, we will learn how to ***save photos and files*** to the *local* directory
+import com.alexeyburak.Bot;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Document;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-If you didn't open the [Lesson 1](sendMessages.md), don't forget to make a `Bot` class with a `token` and `name` of the bot, as well as connect libraries.
+import java.io.File;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
 
-This lesson will present *the simplest* possible *solution* to the problem for the simplicity of the lesson. For better code, these methods would be worth *rewriting*
+/**
+ * Bot tutorial
+ * Created by Alexey Burak
+ */
 
-### Process
-
-The main function remains unchanged, so just copy it in [previous lessons](markups.md)
-
-Let's look at the `HandlerToLessonFive` class
-
-We still need to *Override* `onUpdateReceived` method
-
-For the sake of *simplicity* of the lesson, we will not make any commands. To save the sent document, the user will *only need to send* it to the chat
-
-> Before we started writing methods for *saving*, we will do a simple processing of the received update
-```java
 public class HandleToLessonFive extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
@@ -34,7 +35,7 @@ public class HandleToLessonFive extends TelegramLongPollingBot {
         // Initialize sendMessage
         SendMessage sendMessage = new SendMessage();
         // If update has document
-        if (update.getMessage().hasDocument()) {
+        if (update.getMessage().hasDocument()){
             // Save file method
             saveFile(update, sendMessage);
             // If update has image
@@ -43,19 +44,7 @@ public class HandleToLessonFive extends TelegramLongPollingBot {
             savePhoto(update, sendMessage);
         }
     }
-}
-```
 
-If there is a *document* in the update, method `saveFile` will be called, if there is a *photo*, then method `savePhoto`
-
-Let's start writing the methods themselves
-
-> Let's start by *saving* the file
-
-```java 
-public class HandleToLessonFive extends TelegramLongPollingBot {
-    // Previous methods
-    
     public void saveFile (Update update, SendMessage sendMessage) {
         // Set chat id to send message
         sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
@@ -88,18 +77,6 @@ public class HandleToLessonFive extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
-}
-```
-
-Sending a message here is *optional*, it only serves to *notify* about a successful save
-
-First we use class `Document` to *create* the document itself, then class `GetFile` to *get* this document
-
-> Saving a photo differs only by the *type of object* being created
-
-```java
-public class HandleToLessonFive extends TelegramLongPollingBot {
-    // Previous methods
 
     private void savePhoto(Update update, SendMessage sendMessage) {
         // Set chat id to send message
@@ -127,22 +104,3 @@ public class HandleToLessonFive extends TelegramLongPollingBot {
         }
     }
 }
-```
-
-First we create a `List` of photos, then we get *the largest photo* from this list, then we get the `file id` of this photo.
-Then the same saving procedure is performed as in the *previous method*
-
-This completes the execution of the bot to save the sent photos or documents!
-
-You can find all the code [here](https://github.com/alexeyburak/telegram-bot-tutorial/tree/main/src/com/alexeyburak/lesson5)
-
-> *Sending* documents
-
-![Show Work](images/lesson5/example.png)
-
-> Document *in the local directory*
-
-![Show Work](images/lesson5/showSave.png)
-
-
-Back to [Lesson 4](sendMusic.md) of sending music from the local directory  
